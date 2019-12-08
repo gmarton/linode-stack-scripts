4894b9e0be44dc31ab39c5b1d6c8e00f0f369daa
gmarton <gmarton@gmail.com>
2019-12-08 15:23:55 -0500

    security, db settings

--- LAMP_Debian10.sh
+++ LAMP_Debian10.sh
@@ -1,62 +1,74 @@
 #!/bin/bash
 
-#<UDF name="newuser" Label="New user" example="username" />
-#<UDF name="newpassword" Label="New user password" example="password" />
-#<UDF name="hostname" Label="Hostname" example="example" />
-#<UDF name="domain" Label="Domain" example="example.com" />
+#<UDF name="NEWUSER" Label="New user" example="username" />
+#<UDF name="NEWPASSWORD" Label="New user password" example="password" />
+#<UDF name="SSHKEY" Label="User SSH Key (from ~/.ssh/id_rsa.pub)" />
+#<UDF name="NEWHOSTNAME" Label="NEWHOSTNAME" example="example" />
+#<UDF name="DOMAINNAME" Label="DOMAINNAME" example="example.com" />
 
-# <UDF name="dbpass" Label="MySQL root Password" />
-# <UDF name="dbname" Label="Create Database" default="" example="Create database" />
+# <UDF name="DBPASS" Label="MySQL root Password" />
+# <UDF name="DBNAME" Label="Create Database" default="" example="Create database" />
 
+# Harden SSH
+sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd
+passwd --lock root
 
 # Update system
 apt-get -o Acquire::ForceIPv4=true update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc
 apt-get -o Acquire::ForceIPv4=true update -y
 
-# Add newuser and append to sudoer file
+# Add NEWUSER and append to sudoer file
+apt-get -y install sudo
 adduser $NEWUSER --disabled-password --gecos "" && \
-echo "$NEWUSER:$NEWPASSWORD" | chpasswd
-echo "$NEWUSER  ALL=(ALL:ALL) ALL" >> /etc/sudoers
+	echo "$NEWUSER:$NEWPASSWORD" | chpasswd
+usermod -aG sudo $NEWUSER
+# echo "$NEWUSER  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
-# Set hostname
-hostnamectl set-hostname $HOSTNAME
-echo "127.0.0.1   $HOSTNAME" >> /etc/hosts
+SSHDIR="/home/$NEWUSER/.ssh"
+mkdir $SSHDIR && echo "$SSHKEY" >> $SSHDIR/authorized_keys
+chmod -R 700 $SSHDIR && chmod 600 $SSHDIR/authorized_keys
+chown -R $NEWUSER:$NEWUSER $SSHDIR
+
+# Set NEWHOSTNAME
+NEWHOSTNAMEctl set-hostname $NEWHOSTNAME
+echo "127.0.0.1   $NEWHOSTNAME" >> /etc/hosts
 
 
 # Install Apache
 apt-get install apache2 -y
-
 
 # Edit apache config
 sed -ie "s/KeepAlive Off/KeepAlive On/g" /etc/apache2/apache2.conf
 
 
 # Create a copy of the default Apache configuration file for your site:
-cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$DOMAIN.conf
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$DOMAINNAME.conf
 
 
 # Configuration of vhost file
-cat <<END >/etc/apache2/sites-available/$DOMAIN.conf
-<Directory /var/www/html/$DOMAIN/public_html>
+cat <<END >/etc/apache2/sites-available/$DOMAINNAME.conf
+<Directory /var/www/html/$DOMAINNAME/public_html>
     Require all granted
 </Directory>
 <VirtualHost *:80>
-        ServerName $DOMAIN
-        ServerAlias www.$DOMAIN
-        ServerAdmin webmaster@$DOMAIN
-        DocumentRoot /var/www/html/$DOMAIN/public_html
+        ServerName $DOMAINNAME
+        ServerAlias www.$DOMAINNAME
+        ServerAdmin webmaster@$DOMAINNAME
+        DocumentRoot /var/www/html/$DOMAINNAME/public_html
 
-        ErrorLog /var/www/html/$DOMAIN/logs/error.log
-        CustomLog /var/www/html/$DOMAIN/logs/access.log combined
+        ErrorLog /var/www/html/$DOMAINNAME/logs/error.log
+        CustomLog /var/www/html/$DOMAINNAME/logs/access.log combined
 
 </VirtualHost>
 END
 
-mkdir -p /var/www/html/$DOMAIN/{public_html,logs}
+mkdir -p /var/www/html/$DOMAINNAME/{public_html,logs}
 
 
-cat <<END >/var/www/html/$DOMAIN/public_html/index.html
+cat <<END >/var/www/html/$DOMAINNAME/public_html/index.html
 <!DOCTYPE html>
 <html>
 <head>
@@ -79,7 +91,7 @@ rm /var/www/html/index.html
 
 
 # Enable your virtual host:
-sudo a2ensite $DOMAIN.conf
+sudo a2ensite $DOMAINNAME.conf
 
 
 # Disable the default virtual host
@@ -96,13 +108,26 @@ apt-get install -y certbot python-certbot-apache
 apt-get install -y composer
 
 # Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
-echo "mysql-server mysql-server/root_password password $DBPASS" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password $DBPASS" | sudo debconf-set-selections
-apt-get -y install default-mysql-server
+apt-get install -y default-mysql-server
+systemctl start mariadb
+systemctl enable mariadb
+mysql_secure_installation <<EOF
+y
+$DBPASS
+$DBPASS
+y
+y
+y
+y
+EOF
+
+
+mysql -e "update mysql.user set plugin='' where User='root'"
+mysql -uroot -p$DBPASS -e "flush privileges"
 
 mysql -uroot -p$DBPASS -e "create database $DBNAME"
 
-service mysql restart
+systemctl restart mariadb.service
 
 # Install php
 apt install -y php libapache2-mod-php php-mysql php-pear php-zip php-curl php-xmlrpc php-gd php-mbstring php-xml
@@ -110,7 +135,7 @@ apt install -y php libapache2-mod-php php-mysql php-pear php-zip php-curl php-xm
 # Enable index.php
 sed -ie "s/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml indem/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g" /etc/apache2/mods-enabled/dir.conf
 
-cat <<END >/var/www/html/$DOMAIN/public_html/info.php
+cat <<END >/var/www/html/$DOMAINNAME/public_html/info.php
 <?php phpinfo(); ?>
 END
 
@@ -118,3 +143,12 @@ END
 mkdir /var/log/php
 chown www-data /var/log/php
 systemctl restart apache2
+
+# Firewall
+apt-get install ufw -y
+ufw default allow outgoing
+ufw default deny incoming
+ufw allow 22
+ufw allow http
+ufw allow https
+ufw enable
